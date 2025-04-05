c17c4e33fa5c9393c258e3ebf70b5a03369a53da
Gayatri Lande <gayatrilande15@gmail.com>
2025-04-05 23:55:56 +0530

    Add files via upload

---
+++ MultipleClass.java
@@ -0,0 +1,31 @@
+class MultipleClass
+{
+
+	public static void main(String[] args) 
+		{
+			System.out.println("from class multiple");
+			
+		}
+
+}
+
+class A
+{
+	public static void main(String[] args) {
+		System.out.println("from class A");
+	}
+}
+
+class B
+{
+	public static void main(String[] args) {
+		System.out.println("from class B");
+	}
+}
+
+class c 
+{
+	public static void main(String[] args) {
+		System.out.println("from class C");
+	}
+}
---
+++ PublicClass.java
@@ -0,0 +1,22 @@
+ public class PublicClass 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("from public class ");
+		
+	}
+}
+
+class A
+{
+	public static void main(String[] args) {
+		System.out.println("from class A");
+	}
+}
+
+class B
+{
+	public static void main(String[] args) {
+		System.out.println("from class B");
+	}
+}
